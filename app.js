8d25ab793a90e5281633282d8eea1a5e4c0fae16
nazar-kushniruk <kushniruknazar@gmail.com>
2018-11-29 23:10:56 +0200

    add filter, add delete one item and all items

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 //Ui vars
+"use strict";
+
 const taskForm = document.querySelector('#task-form');
 const taskFilter = document.querySelector('#filter');
 const deleteButton = document.querySelector('.button .clear-tasks');
 const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.list');
-
-// Add all event liseners
+const tasks = document.querySelectorAll('.list-item');
+// Add all event listeners
 loadEventListeners();
 
 function loadEventListeners() {
     taskForm.addEventListener('submit', addTask);
+    taskList.addEventListener('click', taskDelete);
+    taskFilter.addEventListener('keyup', filterTask);
 }
 
 function addTask(e) {
     if (taskInput.value === '') {
-        alert('Add Task !!!')
+        alert('Add Task !!!');
     }
-    
+
     const taskItem = document.createElement('div');
     taskItem.className = 'list-item';
     taskItem.appendChild(document.createTextNode(taskInput.value));
@@ -24,9 +28,33 @@ function addTask(e) {
     deleteItem.className = 'x';
     deleteItem.appendChild(document.createTextNode('x'));
     taskItem.appendChild(deleteItem);
-    console.log(taskItem);
-    console.log(taskList);
+    //  console.log(taskItem);
+    //  console.log(taskList);
     taskList.appendChild(taskItem);
     taskInput.value = '';
     e.preventDefault();
+}
+
+function taskDelete(e) {
+    if (e.target.parentElement.classList.contains('list-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.remove();
+        }
+    }
+}
+
+function filterTask(e) {
+
+    const text = e.target.value.toLowerCase();
+    console.log(text);
+    tasks.forEach(task => {
+        console.log(task.firstChild.textContent);
+        const taskValue = task.firstChild.textContent.toLowerCase();
+        console.log(taskValue.indexOf(text));
+        if (taskValue.indexOf(text) !== -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    })
 }
